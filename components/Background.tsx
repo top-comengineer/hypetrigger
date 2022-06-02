d6690ed7a44eea49a4d14c3e4e4eaf9f3d62e869
Supernova <yunlong870924@gmail.com>
2022-06-02 17:35:02 -0500

    feat(background): Add parallax

--- components/Background.tsx
+++ components/Background.tsx
@@ -1,13 +1,28 @@
+import { useState, useEffect } from 'react'
 import styles from '../styles/Background.module.scss'
 
 export type BackgroundProps = {
   src: string
 }
 
+export const PARALLAX = 0.4
 export default function Background({ src }: BackgroundProps) {
+  const [scrollY, setScrollY] = useState(0)
+
+  useEffect(() => {
+    const onScroll = () => setScrollY(window.scrollY)
+    window.addEventListener('scroll', onScroll)
+    return () => window.removeEventListener('scroll', onScroll)
+  }, [scrollY])
+
   return (
     <div className={styles.background}>
-      <img alt="" src={src} className={styles.img} />
+      <img
+        alt=""
+        src={src}
+        className={styles.img}
+        style={{ marginTop: `${scrollY * PARALLAX}px` }}
+      />
       <div className={styles.gradient} />
     </div>
   )
