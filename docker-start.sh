067136dced62f579c5cf5340566c7ae33a1db5b0
EC2 Default User <ec2-user@ip-172-31-24-224.ec2.internal>
2022-06-04 19:08:53 +0000

    fix(nginx): Fix nginx path from hypetrigger to hypetrigger-site

--- docker-start.sh
+++ docker-start.sh
@@ -5,9 +5,9 @@ sudo docker rm -f ${app}
 sudo docker run -d \
   -p 80:80 \
   -p 443:443 \
-  -v ~/hypetrigger/cert/:/etc/ssl/cert/ \
-  -v ~/hypetrigger/nginx-custom.conf:/etc/nginx/conf.d/nginx-custom.conf:ro \
-  -v ~/hypetrigger/out/:/var/www/html/static/:ro \
+  -v ~/hypetrigger-site/cert/:/etc/ssl/cert/ \
+  -v ~/hypetrigger-site/nginx-custom.conf:/etc/nginx/conf.d/nginx-custom.conf:ro \
+  -v ~/hypetrigger-site/out/:/var/www/html/static/:ro \
   --name=${app} \
   --restart=always \
   --log-opt max-size=100m \
