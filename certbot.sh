78cf840fd61a94de4c782a8520228b86de9ba89e
EC2 Default User <ec2-user@ip-172-31-24-224.ec2.internal>
2022-07-14 19:27:26 +0000

    feat(certbot): Add certbot.sh script to renew letsencrypt certs

--- certbot.sh
+++ certbot.sh
@@ -1,11 +1,11 @@
 # pre-install: backup old certs
-sudo cp -f ~/hypetrigger/cert/fullchain.pem ~/hypetrigger/cert/fullchain-old.pem
-sudo cp -f ~/hypetrigger/cert/privkey.pem ~/hypetrigger/cert/privkey-old.pem
+sudo cp -f ~/hypetrigger-site/cert/fullchain.pem ~/hypetrigger-site/cert/fullchain-old.pem
+sudo cp -f ~/hypetrigger-site/cert/privkey.pem ~/hypetrigger-site/cert/privkey-old.pem
 sudo cp -rf /etc/letsencrypt/live/hypetrigger.io-000* /etc/letsencrypt/live/hypetrigger.io-old
 
 # Remove current certs
-sudo rm -rf ~/hypetrigger/cert/fullchain.pem
-sudo rm -rf ~/hypetrigger/cert/privkey.pem
+sudo rm -rf ~/hypetrigger-site/cert/fullchain.pem
+sudo rm -rf ~/hypetrigger-site/cert/privkey.pem
 #sudo rm -rf /etc/letsencrypt/live/hypetrigger.io-000*
 sudo find /etc/letsencrypt/live -name "hypetrigger.io-000*" -exec rm -rf '{}' \;
 
@@ -21,10 +21,10 @@ docker run -it --rm --name letsencrypt \
         --server https://acme-v02.api.letsencrypt.org/directory
 
 # post-install: copy certs over
-#sudo cp -f /etc/letsencrypt/live/hypetrigger.io-000*/fullchain.pem ~/hypetrigger/cert/fullchain.pem
-#sudo cp -f /etc/letsencrypt/live/hypetrigger.io-000*/privkey.pem ~/hypetrigger/cert/privkey.pem
-sudo find /etc/letsencrypt/live -name "hypetrigger.io-000*/fullchain.pem" -exec cp -f '{}' ~/hypetrigger/cert/fullchain.pem \;
-sudo find /etc/letsencrypt/live -name "hypetrigger.io-000*/privkey.pem" -exec cp -f '{}' ~/hypetrigger/cert/privkey.pem \;
+#sudo cp -f /etc/letsencrypt/live/hypetrigger.io-000*/fullchain.pem ~/HypeTrigger/cert/fullchain.pem
+#sudo cp -f /etc/letsencrypt/live/hypetrigger.io-000*/privkey.pem ~/HypeTrigger/cert/privkey.pem
+sudo find /etc/letsencrypt/live -name "hypetrigger.io-000*/fullchain.pem" -exec cp -f '{}' ~/hypetrigger-site/cert/fullchain.pem \;
+sudo find /etc/letsencrypt/live -name "hypetrigger.io-000*/privkey.pem" -exec cp -f '{}' ~/hypetrigger-site/cert/privkey.pem \;
 
 # docker restart nginx
 echo "Double check certs copied and restart docker."
